5618125b4a9d4ca828f2c0428e2756e31a9de75b
Hiroyoshi Matsumoto <bardiche.assault7@gmail.com>
2022-09-13 20:02:17 +0900

    ♻️ 実行時間の出力内容を多少いい感じに

--- sauna_crawler/main.py
+++ sauna_crawler/main.py
@@ -2,6 +2,7 @@ from original_logger import OriginalLogger
 from crawler.saunaikitai_crawler import SaunaikitaiCrawler
 from crawler.saunaikitai_detail_crawler import SaunaikitaiDetailCrawler
 import time
+import datetime
 
 logger = OriginalLogger()
 logger.info("start")
@@ -14,7 +15,7 @@ detail_crawler = SaunaikitaiDetailCrawler()
 crawler.crawl()
 # detail_crawler.crawl('https://sauna-ikitai.com/saunas/4393')
 
-elapsed_time = time.time() - start_time
+elapsed_time = datetime.timedelta(seconds=time.time() - start_time)
 
-logger.info("done! {} sec".format(elapsed_time))
+logger.info("done! {} sec".format(str(elapsed_time)))
 
